87c13aa53b980fbfb83e9e9519d6b008fdc17047
thebia42 <bianca@LAPTOPB.localdomain>
2020-11-18 20:00:21 +0100

    - show lessons as table - show homework in addition to lesson name - show passed lessons and change their background - don't use substxt for displaying lessons

--- MMM-Webuntis.js
+++ MMM-Webuntis.js
@@ -16,87 +16,111 @@ Module.register("MMM-Webuntis", {
 	},
 
 	getStyles: function () {
-  		return ["MMM-Webuntis.css"];
-  	},
+		return ["MMM-Webuntis.css"];
+	},
 
 	getTranslations: function () {
-  		return {
-  			en: "translations/en.json",
-  			de: "translations/de.json"
-  		};
-  	},
+		return {
+			en: "translations/en.json",
+			de: "translations/de.json"
+		};
+	},
 
-	start: function (){
+	start: function () {
 		this.lessonsByStudent = [];
 		this.sendSocketNotification("FETCH_DATA", this.config);
 	},
 
-	getDom: function() {
+	getDom: function () {
 		var wrapper = document.createElement("div");
 
 		var table = document.createElement("table");
-    		table.className = "bright small light";
+		table.className = "bright small light";
 
 		// no student
 		if (this.lessonsByStudent === undefined) {
-    			return table;
-    		}
+			return table;
+		}
 
 		var addedRows = 0;
 
-		// iterate through students
-		// TODO: for..in does not guarantee specific order
 		for (let studentTitle in this.lessonsByStudent) {
-			//for (const [studentTitle, lessons] of this.lessonsByStudent.entries()) {
-
 			var lessons = this.lessonsByStudent[studentTitle];
 
-			// sort lessons by start time
-			lessons.sort((a,b) => a.sortString - b.sortString);
+			lessons.sort((a, b) => a.lessonSortAlt - b.lessonSortAlt);
 
-			// iterate through lessons of current student
-			for (let i = 0; i < lessons.length; i++) {
-				var lesson = lessons[i];
-				var time = new Date(lesson.year,lesson.month-1,lesson.day,lesson.hour,lesson.minutes);
+			let maxNumberOfLessons = 0;
+			var days = new Map();
 
-				// skip if nothing special
-				if (lesson.code == "") {continue;}
+			lessons.forEach(l => {
+				maxNumberOfLessons = l.lessonNumber > maxNumberOfLessons ? l.lessonNumber : maxNumberOfLessons;
+			});
 
-				// skip past lessons
-				if (time < new Date() && lesson.code != "error") {continue;}
+			lessons.forEach(l => {
+				let day = new Date(l.year, l.month - 1, l.day);
+				let dayLong = day.toLocaleDateString("de-DE", { weekday: "long" });
+				if (!days.get(dayLong)) {
+					console.log("ADD DAY: " + dayLong);
+					let daysLessons = [];
+					daysLessons[l.lessonNumber] = l;
+					days.set(dayLong, daysLessons);
+				} else {
+					days.get(dayLong)[l.lessonNumber] = l;
+				}
+			});
 
-				addedRows++;
+			var headerRow = document.createElement("tr");
+			table.appendChild(headerRow);
 
+			var headerCorner = document.createElement("th");
+			headerCorner.innerHTML = studentTitle;
+			headerRow.appendChild(headerCorner);
+
+			for (const [key, value] of days.entries()) {
+				var header = document.createElement("th");
+				header.innerHTML = key;
+				headerRow.appendChild(header);
+			}
+
+			for (let i = 1; i <= maxNumberOfLessons; i++) {
 				var row = document.createElement("tr");
+				let dateTimeCellCreated = false;
+				for (const [key, value] of days.entries()) {
+					var lesson = value[i];
+					if (lesson != undefined) {
+						var time = new Date(lesson.year, lesson.month - 1, lesson.day, lesson.hour, lesson.minutes);
+						var passed = time < new Date() && lesson.code != "error";
+						if (!dateTimeCellCreated) {
+							// date and time
+							var dateTimeCell = document.createElement("td");
+							if (this.config.showStartTime || lesson.lessonNumber === undefined) { dateTimeCell.innerHTML += time.toLocaleTimeString("de-DE", { hour: "2-digit", minute: "2-digit" }); }
+							else { dateTimeCell.innerHTML += lesson.lessonNumber + "."; }
+							dateTimeCell.className = "leftSpace align-right alignTop";
+							row.appendChild(dateTimeCell);
+							dateTimeCellCreated = true;
+						}
+
+						// subject cell
+						var subjectCell = document.createElement("td");
+						subjectCell.innerHTML =
+							this.capitalize(lesson.subject) + "&nbsp;(" +
+							this.capitalize(lesson.teacher) + ")";
+						if (lesson.homework) {
+							subjectCell.innerHTML += "<br />";
+							subjectCell.innerHTML += lesson.homework;
+						}
+						subjectCell.className = "leftSpace align-left alignTop";
+						if (lesson.code == "cancelled") { subjectCell.className += " cancelled"; }
+						if (lesson.code == "error") { subjectCell.className += " error"; }
+						if (passed) { subjectCell.className += " passed"; }
+						row.appendChild(subjectCell);
+					} else {
+						// empty cell
+					}
+				};
+				addedRows++;
 				table.appendChild(row);
-
-				// title, i.e. class name or child name
-				var titleCell = document.createElement("td");
-				titleCell.innerHTML = studentTitle;
-				titleCell.className = "align-right alignTop";
-				row.appendChild(titleCell);
-
-				// date and time
-				var dateTimeCell = document.createElement("td");
-				dateTimeCell.innerHTML = time.toLocaleDateString("de-DE",{weekday:"short"}).toUpperCase() + "&nbsp;";
-				if (this.config.showStartTime || lesson.lessonNumber === undefined) {dateTimeCell.innerHTML += time.toLocaleTimeString("de-DE", {hour:"2-digit",minute:"2-digit"});}
-				else {dateTimeCell.innerHTML += lesson.lessonNumber + ".";}
-				dateTimeCell.className = "leftSpace align-right alignTop";
-				row.appendChild(dateTimeCell);
-
-				// subject cell
-				var subjectCell = document.createElement("td");
-				subjectCell.innerHTML = lesson.substText;
-				if (lesson.substText == "") {subjectCell.innerHTML =
-                this.capitalize(lesson.subject) + "&nbsp;(" +
-                this.capitalize(lesson.teacher) + ")";}
-				//if (lesson.text.length > 0 ) subjectCell.innerHTML += "</br><span class='xsmall dimmed'>" + lesson.text + "</span>";
-				subjectCell.className = "leftSpace align-left alignTop";
-				if (lesson.code == "cancelled") {subjectCell.className += " cancelled";}
-				if (lesson.code == "error") {subjectCell.className += " error";}
-
-				row.appendChild(subjectCell);
-			} // end for lessons
+			}
 		} // end for students
 
 		// add message row if table is empty
@@ -113,30 +137,30 @@ Module.register("MMM-Webuntis", {
 		return wrapper;
 	},
 
-	capitalize: function(str) {
+	capitalize: function (str) {
 		str = str.toLowerCase().split(" ");
 
 		for (let i = 0, x = str.length; i < x; i++) {
 			if (str[i]) {
-				if (str[i] === "ii" || str[i] === "iii") {str[i] = str[i].toUpperCase();}
-				else {str[i] = str[i][0].toUpperCase() + str[i].substr(1);}
+				if (str[i] === "ii" || str[i] === "iii") { str[i] = str[i].toUpperCase(); }
+				else { str[i] = str[i][0].toUpperCase() + str[i].substr(1); }
 			}
 		}
 
 		return str.join(" ");
 	},
 
-	notificationReceived: function(notification, payload) {
-		switch(notification) {
-		case "DOM_OBJECTS_CREATED":
-			var timer = setInterval(() => {
-				this.sendSocketNotification("FETCH_DATA", this.config);
-			}, this.config.fetchInterval);
-			break;
+	notificationReceived: function (notification, payload) {
+		switch (notification) {
+			case "DOM_OBJECTS_CREATED":
+				var timer = setInterval(() => {
+					this.sendSocketNotification("FETCH_DATA", this.config);
+				}, this.config.fetchInterval);
+				break;
 		}
 	},
 
-	socketNotificationReceived: function(notification, payload) {
+	socketNotificationReceived: function (notification, payload) {
 		if (notification === "GOT_DATA") {
 			if (payload.lessons) {
 				this.lessonsByStudent[payload.title] = payload.lessons;
--- node_helper.js
+++ node_helper.js
@@ -2,10 +2,10 @@ const NodeHelper = require("node_helper");
 const WebUntis = require("webuntis");
 
 module.exports = NodeHelper.create({
-	start: function() {
+	start: function () {
 	},
 
-	socketNotificationReceived: function(notification, payload) {
+	socketNotificationReceived: function (notification, payload) {
 
 		if (notification === "FETCH_DATA") {
 
@@ -16,7 +16,7 @@ module.exports = NodeHelper.create({
 		}
 	},
 
-	fetchLessons: function(studentData, days) {
+	fetchLessons: function (studentData, days) {
 
 		const untis = new WebUntis(
 			studentData.school,
@@ -25,7 +25,7 @@ module.exports = NodeHelper.create({
 			studentData.server
 		);
 
-		if (days<1 || days>10 || isNaN(days)) {days = 1;}
+		if (days < 1 || days > 10 || isNaN(days)) { days = 1; }
 
 		// create lessons array to be sent to module
 		var lessons = [];
@@ -33,12 +33,17 @@ module.exports = NodeHelper.create({
 		// array to get lesson number by start time
 		var startTimes = [];
 
+		var rangeStart = new Date();
+		var rangeEnd = new Date();
+
+		let responseData = [];
+
 		untis
 			.login()
 			.then(response => {
-				var rangeStart = new Date();
-				var rangeEnd = new Date();
-				rangeEnd.setDate(rangeStart.getDate()+days);
+				rangeStart.setHours(0, 0, 0, 0);
+				rangeEnd.setDate(rangeStart.getDate() + days);
+				rangeEnd.setHours(23,59,59,0);
 
 				untis.getTimegrid()
 					.then(grid => {
@@ -55,38 +60,76 @@ module.exports = NodeHelper.create({
 				return untis.getOwnTimetableForRange(rangeStart, rangeEnd);
 			})
 			.then(timetable => {
+				responseData[0] = timetable;
+				return untis.getHomeWorksFor(rangeStart, rangeEnd);
+			})
+			.then(homework => {
+				responseData[1] = homework;
 
-				timetable.forEach(element => {
-					let lesson = {};
-					let year = element.date.toString().substring(0,4);
-					let month = element.date.toString().substring(4,6);
-					let day = element.date.toString().substring(6);
-					let hour = element.startTime.toString();
-					hour.length == 3 ? hour = "0"+hour.substring(0,1) : hour = hour.substring(0,2);
-					let minutes = element.startTime.toString()
-					minutes = minutes.substring(minutes.length-2);
-					lesson.sortString = element.date + hour + minutes;
-					switch (element.code) {
-					case "cancelled": lesson.sortString += "1"; break;
-					case "irregular": lesson.sortString += "2"; break;
-					default: lesson.sortString += "9";
-					}
-					lesson.year = year;
-					lesson.month = month;
-					lesson.day = day;
-					lesson.hour = hour;
-					lesson.minutes = minutes;
-					lesson.lessonNumber = startTimes[element.startTime];
-					element.su[0] ? lesson.subject = element.su[0].longname : lesson.subject = "";
-					element.te[0] ? lesson.teacher = element.te[0].longname : lesson.teacher = "";
-					element.code ? lesson.code = element.code : lesson.code = "";
-					element.lstext ? lesson.text = element.lstext : lesson.text = "";
-					element.substText ? lesson.substText = element.substText : lesson.substText = "";
+				let homeworkMap = new Map();
 
-					lessons.push(lesson);
-				});
+				if (responseData[1]) {
+					var lessonMap = new Map();
+					responseData[1].lessons.forEach(l => {
+						lessonMap.set(l.id, l.subject);
+					});
+					responseData[1].homeworks.forEach(h => {
+						let year = h.dueDate.toString().substring(0, 4);
+						let month = h.dueDate.toString().substring(4, 6);
+						let day = h.dueDate.toString().substring(6);
+						let key = year + month + day; 
+						let entries = lessonMap.get(key);
+						if (!entries) {
+							entries = new Map();
+						}
+						entries.set(lessonMap.get(h.lessonId), h.text);
+						homeworkMap.set(key, entries);
+					});
+				}
+				if (responseData[0]) {
+					let timetable = responseData[0];
+					timetable.forEach(element => {
+						let lesson = {};
+						let year = element.date.toString().substring(0, 4);
+						let month = element.date.toString().substring(4, 6);
+						let day = element.date.toString().substring(6);
+						let hour = element.startTime.toString();
+						let dueDate = year + month + day;
+						hour.length == 3 ? hour = "0" + hour.substring(0, 1) : hour = hour.substring(0, 2);
+						let minutes = element.startTime.toString()
+						minutes = minutes.substring(minutes.length - 2);
+						lesson.sortString = element.date + hour + minutes;
+						switch (element.code) {
+							case "cancelled": lesson.sortString += "1"; break;
+							case "irregular": lesson.sortString += "2"; break;
+							default: lesson.sortString += "9";
+						}
+						lesson.year = year;
+						lesson.month = month;
+						lesson.day = day;
+						lesson.hour = hour;
+						lesson.minutes = minutes;
+						lesson.lessonNumber = startTimes[element.startTime];
+						lesson.lessonSortAlt = lesson.lessonNumber + " " + new Date(year, month - 1, hour).getDay();
+						element.su[0] ? lesson.subject = element.su[0].longname : lesson.subject = "";
+						element.te[0] ? lesson.teacher = element.te[0].longname : lesson.teacher = "";
+						element.code ? lesson.code = element.code : lesson.code = "";
+						element.lstext ? lesson.text = element.lstext : lesson.text = "";
+						element.substText ? lesson.substText = element.substText : lesson.substText = "";
 
-				this.sendSocketNotification("GOT_DATA", {title: studentData.title, lessons: lessons});
+						let homeworkForDate = homeworkMap.get(dueDate);
+						if (homeworkForDate != undefined) {
+							let entry = homeworkForDate.get(element.su[0].name);
+							lesson.homework = entry;
+						}
+
+						console.log("LESSON: \nyear: " + lesson.year + "\nmonth: " + lesson.month + "\nday: " + lesson.day + "\nsubject: " + lesson.subject +
+							"\nteacher: " + lesson.teacher + "\ncode: " + lesson.code + "\nhomework: " + lesson.homework);
+						lessons.push(lesson);
+					});
+				}
+
+				this.sendSocketNotification("GOT_DATA", { title: studentData.title, lessons: lessons });
 			})
 			.catch(error => {
 				console.log("ERROR for " + studentData.title + ": " + error.toString());
@@ -102,8 +145,8 @@ module.exports = NodeHelper.create({
 					teacher: error.toString(),
 					code: "error"
 				} ];
-        		this.sendSocketNotification("GOT_DATA", {title: studentData.title, lessons: errorObject});
-        		*/
+				this.sendSocketNotification("GOT_DATA", {title: studentData.title, lessons: errorObject});
+				*/
 			});
 
 		untis.logout();
